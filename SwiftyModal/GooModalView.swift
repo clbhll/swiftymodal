4800c213efc4a2c78a84186968026c7784d11290
caleb hill <hello@calebhill.me>
2023-03-24 23:48:13 -0700

    GooModalView file created for next steps

---
+++ SwiftyModal/GooModalView.swift
@@ -0,0 +1,13 @@
+//
+//  GooModalView.swift
+//  SwiftyModal
+//
+//  Created by Caleb Hill on 3/24/23.
+//
+
+import SwiftUI
+import UIKit
+
+
+// modal view with custom detents will go here
+// will explore options to create a detent that sizes based on child content
