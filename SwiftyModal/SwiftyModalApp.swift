796fef2e6eb304e7707f2d201fbf9ddda3b030bc
caleb hill <hello@calebhill.me>
2023-03-24 23:07:08 -0700

    Initial Commit

---
+++ SwiftyModal/SwiftyModalApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftyModalApp.swift
+//  SwiftyModal
+//
+//  Created by Caleb Hill on 3/24/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftyModalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
